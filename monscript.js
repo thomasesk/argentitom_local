425e3cef5eac9973c7d7796b5a29ebce017b4711
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2022-02-08 20:41:58 +0100

    Repo Import from Desktop

---
+++ monscript.js
@@ -0,0 +1,23 @@
+document.getElementById("outback-link").addEventListener("mouseover", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/australian_road.jpeg)";
+}, false);
+
+document.getElementById("outback-link").addEventListener("mouseout", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/photo_bidart_plage_noir_et_blanc.jpg)";
+}, false);
+
+document.getElementById("pays-basque-link").addEventListener("mouseover", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/photo_bidart_plage_noir_et_blanc.jpg)";
+}, false);
+
+document.getElementById("pays-basque-link").addEventListener("mouseout", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/photo_bidart_plage_noir_et_blanc.jpg)";
+}, false);
+
+document.getElementById("bondi-beach-link").addEventListener("mouseover", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/lonely_surfer.jpg)";
+}, false);
+
+document.getElementById("bondi-beach-link").addEventListener("mouseout", function() {
+  document.getElementById("banner-image").style.backgroundImage = "url(images/photo_bidart_plage_noir_et_blanc.jpg)";
+}, false);
